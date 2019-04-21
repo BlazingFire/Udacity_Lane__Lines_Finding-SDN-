e2b4bdb43a48ef76ec8e8dcad7159b0f76365ddc
BlazingFIre <abhi.jaiswal44@gmail.com>
2019-04-21 16:50:23 +0000

    hough paramter tuning

--- set_git.sh
+++ set_git.sh
@@ -6,20 +6,20 @@ git pull
 echo
 
 # Ask the user for login details
-read -p 'Git repository url: ' upstreamVar
-read -p 'Git Username: ' userVar
-read -p 'Git email: ' emailVar
+#read -p 'Git repository url: ' upstreamVar
+#read -p 'Git Username: ' userVar
+#read -p 'Git email: ' emailVar
 
 echo
-echo Thank you $userVar!, we now have your credentials
-echo for upstream $upstreamVar. You must supply your password for each push.
+#echo Thank you $userVar!, we now have your credentials
+#echo for upstream $upstreamVar. You must supply your password for each push.
 echo
 
 echo setting up git
 
-git config --global user.name $userVar
-git config --global user.email $emailVar
-git remote set-url origin $upstreamVar
+git config --global user.name 'BlazingFIre'
+git config --global user.email 'abhi.jaiswal44@gmail.com'
+#git remote set-url origin $upstreamVar
 echo
 
 echo Please verify remote:
